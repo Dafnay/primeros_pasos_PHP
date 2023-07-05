15c34defd1717a1ecfe401c612f14b8549cd83d3
Alba Velasco <albavelascoglez@gmail.com>
2023-07-05 16:50:47 +0200

    functions done

--- variables.php
+++ variables.php
@@ -34,6 +34,8 @@ print_r($hobbies);
 echo "<br>"; 
 var_dump($puede_volar);
 echo "<br>"; 
+echo "<br>";
+echo "<br>";
 // <!-- ¿Qué tipo de dato son las siguientes variables? -->
 
 //Fácil
@@ -49,6 +51,8 @@ var_dump($edad);
 echo "<br>"; 
 var_dump($Ravenclaw);
 echo "<br>";
+echo "<br>";
+echo "<br>";
 
 //Medio
 $promedio = (8 + 9.5 + 9 + 10 + 8) / 5;
@@ -62,6 +66,8 @@ echo "<br>";
 var_dump($apellido);
 echo "<br>"; 
 var_dump($presento_examen);
+echo "<br>";
+echo "<br>";
 echo "<br>";
 
 
@@ -81,6 +87,8 @@ var_dump($promedio_maximo);
 echo "<br>"; 
 var_dump($nargles);
 echo "<br>";
+echo "<br>";
+echo "<br>";
 
 //Suponiendo estas variables
 $es_un_michi_grande = true;
@@ -99,5 +107,90 @@ echo "<br>";
 var_dump(!$le_gusta_comer);
 echo "<br>";
 var_dump(!$le_gusta_comer || $es_un_michi_grande);
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+
+// Convierte todos los caracteres del string a mayúsculas e imprimir en pantalla, sin modificar la
+// variable original.
+
+$character = 'hermione';
+$upper = strtoupper($character);
+var_dump($upper);
+echo "<br>";
+
+// Imprime en pantalla la longitud de la variable tipo string.
+
+$longitud = strlen($character);
+var_dump($lenght);
+echo "<br>";
+
+// Imprime en pantalla la cantidad de palabras de la variable tipo string.
+$words = str_word_count($character);
+var_dump($words);
+echo "<br>";
+
+// Cambia a orden inverso los caracteres de la variable string e imprime en pantalla, sin modificar la
+// variable original.
+
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+// Reemplaza la palabra 'Gato' de tu variable original a 'Michi' e imprime en pantalla.
+// Declara dos variables y dale un valor de tipo integer e imprime ambas en pantalla
+$a = 5;
+$b = 7;
+var_dump($a,$b);
+echo "<br>";
+// Suma ambas variables de tipo integer e imprime en pantalla.
+$addition = $a + $b;
+var_dump($addition);
+echo "<br>";
+// Declara dos variables y dale un valor de 1 y 0 tipo integer e imprime el tipo de dato en pantalla y
+// consola
+$c = 1;
+$d = 0;
+var_dump($c, $d);
+echo "<br>";
+// a las variables anteriores cambiales el tipo de dato a Booleano e imprime en el nuevo tipo de dato
+// en pantalla y consola.
+
+settype($c, 'boolean');
+settype($d, 'boolean');
+var_dump($c);
+echo "<br>";
+var_dump($d);
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+
+// Crear función que reciba como parámetros dos números y que retorne la suma de ellos. Imprime
+// en pantalla y por consola el resultado
+
+function add($num1, $num2) {
+    $res = $num1 + $num2;
+    return $res;
+}
+
+var_dump( add( 5, 5));
+echo "<br>";
+
+
+// Crear función que determine si un número es par o impar. Imprime por consola y pantalla
+
+function esPar($number) {
+    if ($number % 2 == 0){
+        echo "El número  " . $number . " es PAR";
+    } else {
+        echo "El número  " . $number . " es IMPAR";
+    }
+}
+
+$number = 42;
+esPar($number);
+
 ?>
 
