d23373b4277403d80ac55a609291841786c91eca
Alba Velasco <albavelascoglez@gmail.com>
2023-07-05 17:27:12 +0200

    funciones.php approach

--- funciones.php
+++ funciones.php
@@ -1,30 +1,11 @@
-
 <?php
 
+// Crear una función que imprima en consola y pantalla todos los números de un array en una lista
+$number = [1, 2, 3, 4, 5, 6];
+echo('<pre>');
+print_r($number);
+echo('</pre>');
 
-// Crear función que reciba como parámetros dos números y que retorne la suma de ellos. Imprime
-// en pantalla y por consola el resultado
-
-function add($num1, $num2) {
-    $res = $num1 + $num2;
-    return $res;
-}
-
-var_dump( add( 5, 5));
-echo "<br>";
-
-
-// Crear función que determine si un número es par o impar. Imprime por consola y pantalla
-
-function esPar($number) {
-    if ($number % 2 == 0){
-        echo "El número  " . $number . " es PAR";
-    } else {
-        echo "El número  " . $number . " es IMPAR";
-    }
-}
-
-$number = 42;
-esPar($number);
+// Crear una función que añade un número al array
 
 ?>
