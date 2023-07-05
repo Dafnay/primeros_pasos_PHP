18d44bb0090254bc60a3802fff5f6eb8fa099305
Alba Velasco <albavelascoglez@gmail.com>
2023-07-05 14:36:03 +0200

    variables approach

---
+++ variables.php
@@ -0,0 +1,42 @@
+    
+<?php
+
+
+// <!--Declara una variable por cada tipo de dato y asignar un valor -->
+
+//  <!-- STRING-->
+$name = 'Alba';  
+//  <!-- INTEGER -->            
+$age = 36;  
+//  <!-- FLOAT -->                 
+$float_number = 12.345;  
+//  <!-- BOOLEAN-->    
+$son = true; 
+
+
+//  <!-- ARRAY- -->                
+$hobbies = ['draw', 'read', 'music'];
+// <!-- OBJECT-->
+
+// <!-- NULL -->
+// <!-- Muestra el valor de esa variable en consola y en pantalla (Averigua cómo imprimir el valor
+// booleano (que imprima true o false), un array y un valor nulo (que imprima NULL) -->
+
+echo $name;
+var_dump($name);
+echo "<br>"; 
+echo $age;
+echo var_dump($age);
+echo "<br>"; 
+echo $float_number;
+echo var_dump($float_number);
+echo "<br>"; 
+var_dump($son);
+echo "<br>";
+print_r($hobbies) ;
+echo "<br>"; 
+
+// <!-- ¿Qué tipo de dato son las siguientes variables? -->
+
+
+?>
